3eaefd1fe906f420ae667c3f27e3105a606b75bd
11307703 <11307703@student.pxl.be>
2024-11-23 16:10:36 +0100

    commit1

---
+++ Oefening2/Oefening2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oefening2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            numberTextBox.Focus();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DelenTryCatch();
+           
+        }
+
+        private void DelenTryCatch()
+        {
+
+            try
+            {
+                int result = int.Parse(numberTextBox.Text) / int.Parse(dividerTextBox.Text);
+                resultTextBox.Text = result.ToString();
+
+                if(int.Parse(numberTextBox.Text) > int.MaxValue)
+                {
+                    throw new OverflowException();
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("De deler mag niet 0 zijn", "Fout bij deling",MessageBoxButton.OK);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Je moet 2 gehele getallen ingeven.\n\nDe indeling van de invoertekens is onjuist", "Conversiefout bij DELER/DEELTAL", MessageBoxButton.OK);
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("DEELTAL is te groot!", "Conversiefout",MessageBoxButton.OK);
+            }
+        }
+    }
+}
